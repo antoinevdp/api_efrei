c20355dc14551352b9c5484dd911362f83b50a0f
Antoine <antoinevdp@live.fr>
2025-02-04 14:20:30 +0100

    init commit

---
+++ src/config.mjs
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://avandeplanque:H4lgSd46R6Yy8Yzz@efrei.o39gb.mongodb.net/api'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/ecole'
+  }
+};
